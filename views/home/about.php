54ded63bfba5c75759c974756d1a3a4f1b360f0e
Regemaster <39582966+Regemaster@users.noreply.github.com>
2018-05-24 22:12:34 +0200

    Create about.php

---
+++ views/home/about.php
@@ -0,0 +1,39 @@
+<body id="aboutSite">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+            </div>
+            <div class="col-sm-6">
+                <h2>About Booksaver</h2>
+            </div>
+            <div class="col-sm-3">
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+            </div>
+            <div class="col-sm-6">
+                <div class="informationsTitle">Anytime</div>
+                <div class="informations">
+                    BookSaver allows you to keep your bookmarks always Online,
+                    so no metter where you are, you'r always able to get them.
+                </div>
+
+                <div class="informationsTitle">Anywhere</div>
+                <div class="informations">
+                    BookSaver is responsive, which means that you can get your favorite bookmarks from any device.
+                </div>
+
+                <div class="informationsTitle">Support</div>
+                <div class="informations">
+                    BookSaver is provided as-is with no direct support and no guarantees.
+                    You have to use it at your own risk.
+                </div>
+            </div>
+            <div class="col-sm-3">
+            </div>
+        </div>
+    </div>
+</body>
